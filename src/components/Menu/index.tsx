7dd2d4906bd45058b3209a2ddeb303eacb69660f
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-27 02:02:34 -0300

    feat: add Menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,21 @@
-import { TimerIcon } from "lucide-react";
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
 
 export function Menu() {
   return (
-    <div className={styles.logo}>
-      <a className={styles.logoLink} href='#'>
-        <TimerIcon />
-        <span>Chronodoro</span>
+    <nav className={styles.menu}>
+      <a className={styles.menuLink} href='#'>
+        <HouseIcon />
       </a>
-    </div>
+      <a className={styles.menuLink} href='#'>
+        <HistoryIcon />
+      </a>
+      <a className={styles.menuLink} href='#'>
+        <SettingsIcon />
+      </a>
+      <a className={styles.menuLink} href='#'>
+        <SunIcon />
+      </a>
+    </nav>
   );
 }
