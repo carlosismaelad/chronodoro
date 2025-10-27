446c1a399965c7f9fdddb53721f64d8da92cbdd6
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-27 01:45:18 -0300

    feat: add Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import "./styles/theme.css";
-import "./styles/globals.css";
 import { Container } from "./components/Container";
 import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+
+import "./styles/theme.css";
+import "./styles/globals.css";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>MENU</Heading>
-      </Container>
-      <Container>
-        <Heading>ABOUT</Heading>
       </Container>
     </>
   );
