4d5d62887c6d44318569d6fb453d305a5118ddc3
Carlos Dourado <carlosismaelad@outlook.com>
2025-10-27 01:06:39 -0300

    feat: structures the first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/globals.css";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
     <>
-      <Heading>Olá Mundo 2</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non enim molestias cumque autem iusto porro soluta iste
-        modi exercitationem aperiam ab nam culpa voluptatem, quis provident? Aspernatur, labore debitis maiores ad
-        expedita asperiores doloremque. Laudantium suscipit explicabo, reiciendis quidem ad deleniti nam nemo molestias
-        nulla deserunt, enim repudiandae adipisci impedit.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+      <Container>
+        <Heading>ABOUT</Heading>
+      </Container>
     </>
   );
 }
